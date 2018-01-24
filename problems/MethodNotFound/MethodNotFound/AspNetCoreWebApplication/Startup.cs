302455c7ac1417d73c31606940482cc44da40be3
springy76 <springy@gmx.de>
2018-01-24 22:06:32 +0100

    no problem on W7 :(

---
+++ problems/MethodNotFound/MethodNotFound/AspNetCoreWebApplication/Startup.cs
@@ -0,0 +1,40 @@
+﻿namespace AspNetCoreWebApplication
+{
+	using System.Threading.Tasks;
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.Extensions.Configuration;
+	using Microsoft.Extensions.DependencyInjection;
+
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			this.Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc();
+
+			/* >>>>>> the only code added to original webapi template >>>>>> */
+			services.AddAuthentication()
+				.AddBasicAuthentication(tuple => Task.FromResult(true));
+			/* <<<<<< the only code added to original webapi template <<<<<< */
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if ( env.IsDevelopment() )
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			app.UseMvc();
+		}
+	}
+}
